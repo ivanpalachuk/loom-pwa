b7bf35e9ec3672f7f0bcbf12216eca57b6aead3a
Ivan Palachuk <ivanzar@hotmail.com>
2025-11-25 10:54:04 -0300

    feat: Exclude patron_loom_01.png from Workbox caching in vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -50,6 +50,7 @@ export default defineConfig({
             },
             workbox: {
                 globPatterns: ['**/*.{js,css,html,ico,png,jpg,svg,woff,woff2}'],
+                globIgnores: ['**/patron_loom_01.png'],
                 runtimeCaching: [
                     {
                         urlPattern: /^https:\/\/fonts\.googleapis\.com\/.*/i,
